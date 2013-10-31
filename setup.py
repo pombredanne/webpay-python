06d062c25dd30e6c3c9a77ecbfa2fc7755c4fde5
tomykaira <tomykaira@gmail.com>
2013-11-01 01:13:35 +0900

    Add python support versions

--- setup.py
+++ setup.py
@@ -26,7 +26,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules'
