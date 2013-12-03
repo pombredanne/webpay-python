3a6bb879b0adaf5ed1eaed0f47665cdb7efac8fc
hmsk <k.hamasaki@gmail.com>
2013-12-03 23:00:39 +0900

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.rst') as file:
 setup(
     name='webpay',
     packages=['webpay', 'webpay.api', 'webpay.model'],
-    version='1.0.1',
+    version='1.0.2',
     author='webpay',
     author_email='administrators@webpay.jp',
     url='https://github.com/webpay/webpay-python',
