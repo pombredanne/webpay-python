0fdaf07288a87e1fa91030eb556ca397d42c9cbf
tomykaira <tomykaira@gmail.com>
2014-02-26 00:58:02 +0900

    Fix unicode related error in python 2.7

--- tests/test_webpay.py
+++ tests/test_webpay.py
@@ -21,7 +21,9 @@ class TestWebpay:
             with HTTMock(mock):
                 webpay.customers.retrieve('cus_eS6dGfa8BeUlbS')
         exc = excinfo.value
-        assert exc.__str__() == '該当する顧客がありません: cus_eS6dGfa8BeUlbS'
+        assert exc.args[0].encode('utf-8') == \
+            b'\xe8\xa9\xb2\xe5\xbd\x93\xe3\x81\x99\xe3\x82\x8b\xe9\xa1\xa7\xe5\xae\xa2\xe3\x81' + \
+            b'\x8c\xe3\x81\x82\xe3\x82\x8a\xe3\x81\xbe\xe3\x81\x9b\xe3\x82\x93: cus_eS6dGfa8BeUlbS'
 
     def test_request_raises_not_found_with_default_language(self):
         webpay = WebPay('test_key')
@@ -35,4 +37,4 @@ class TestWebpay:
             with HTTMock(mock):
                 webpay.charges.retrieve('foo')
         exc = excinfo.value
-        assert exc.__str__() == 'No such charge: foo'
+        assert exc.args[0] == 'No such charge: foo'
